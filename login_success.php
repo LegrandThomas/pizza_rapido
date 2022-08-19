1b8f0a051b055025cf8385ed9f188778e160a537
Legrand Thomas <pro.legrand.thomas@gmail.com>
2022-08-19 10:34:33 +0200

    t

--- login_success.php
+++ login_success.php
@@ -4,11 +4,11 @@
  if(isset($_SESSION["username"]))  
  {  
       echo '<h3>Succes, Bienvenue - '.$_SESSION["username"].'</h3>';  
-      echo '<br /><br /><a href="./logout.php">Logout</a>';  
+      echo '<br /><br /><a href="logout.php">Logout</a>';  
       if ($_SESSION["username"]=="admin"){
       echo '<br /><br /><a href="Admin_dashboard.php">Dashboard admin</a>';  
     }else
-    echo '<br /><br /><a href="./commande.php">Commander</a>'; 
+    echo '<br /><br /><a href="commande.php">Commander</a>'; 
  }  
  else  
  {  
